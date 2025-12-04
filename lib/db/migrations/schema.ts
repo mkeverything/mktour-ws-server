721215a6eb2741d7ca92cfea242ee322c05ab7cc
sukalov <matvey10@gmail.com>
2025-12-04 16:12:03 -0700

    style: convert to single quotes and improve code formatting

--- lib/db/migrations/schema.ts
+++ lib/db/migrations/schema.ts
@@ -1,150 +1,191 @@
-import { sqliteTable, AnySQLiteColumn, uniqueIndex, text, integer, foreignKey } from "drizzle-orm/sqlite-core"
-  import { sql } from "drizzle-orm"
+import { sqliteTable, AnySQLiteColumn, uniqueIndex, text, integer, foreignKey } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
 
-export const club = sqliteTable("club", {
-	id: text().primaryKey().notNull(),
-	name: text().notNull(),
-	description: text(),
-	createdAt: integer("created_at").notNull(),
-	lichessTeam: text("lichess_team"),
-},
-(table) => [
-	uniqueIndex("club_lichess_team_unique").on(table.lichessTeam),
-]);
+export const club = sqliteTable(
+  'club',
+  {
+    id: text().primaryKey().notNull(),
+    name: text().notNull(),
+    description: text(),
+    createdAt: integer('created_at').notNull(),
+    lichessTeam: text('lichess_team'),
+  },
+  (table) => [uniqueIndex('club_lichess_team_unique').on(table.lichessTeam)],
+);
 
-export const clubsToUsers = sqliteTable("clubs_to_users", {
-	id: text().primaryKey().notNull(),
-	clubId: text("club_id").notNull().references(() => club.id),
-	userId: text("user_id").notNull().references(() => user.id),
-	status: text().notNull(),
-	promotedAt: integer("promoted_at").notNull(),
+export const clubsToUsers = sqliteTable('clubs_to_users', {
+  id: text().primaryKey().notNull(),
+  clubId: text('club_id')
+    .notNull()
+    .references(() => club.id),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id),
+  status: text().notNull(),
+  promotedAt: integer('promoted_at').notNull(),
 });
 
-export const clubNotification = sqliteTable("club_notification", {
-	id: text().primaryKey().notNull(),
-	createdAt: integer("created_at").notNull(),
-	clubId: text("club_id").notNull().references(() => club.id, { onDelete: "cascade" } ),
-	event: text().notNull(),
-	isSeen: integer("is_seen").notNull(),
-	metadata: text().notNull(),
+export const clubNotification = sqliteTable('club_notification', {
+  id: text().primaryKey().notNull(),
+  createdAt: integer('created_at').notNull(),
+  clubId: text('club_id')
+    .notNull()
+    .references(() => club.id, { onDelete: 'cascade' }),
+  event: text().notNull(),
+  isSeen: integer('is_seen').notNull(),
+  metadata: text().notNull(),
 });
 
-export const userNotification = sqliteTable("user_notification", {
-	id: text().primaryKey().notNull(),
-	createdAt: integer("created_at").notNull(),
-	userId: text("user_id").notNull().references(() => user.id, { onDelete: "cascade" } ),
-	event: text().notNull(),
-	isSeen: integer("is_seen").notNull(),
-	metadata: text().notNull(),
+export const userNotification = sqliteTable('user_notification', {
+  id: text().primaryKey().notNull(),
+  createdAt: integer('created_at').notNull(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id, { onDelete: 'cascade' }),
+  event: text().notNull(),
+  isSeen: integer('is_seen').notNull(),
+  metadata: text().notNull(),
 });
 
-export const affiliation = sqliteTable("affiliation", {
-	id: text().primaryKey().notNull(),
-	userId: text("user_id").notNull().references(() => user.id, { onDelete: "cascade" } ),
-	clubId: text("club_id").notNull().references(() => club.id, { onDelete: "cascade" } ),
-	playerId: text("player_id").notNull().references(() => player.id, { onDelete: "cascade" } ),
-	status: text().notNull(),
-	createdAt: integer("created_at").notNull(),
-	updatedAt: integer("updated_at").notNull(),
-},
-(table) => [
-	uniqueIndex("affiliation_user_club_unique_idx").on(table.userId, table.clubId),
-]);
+export const affiliation = sqliteTable(
+  'affiliation',
+  {
+    id: text().primaryKey().notNull(),
+    userId: text('user_id')
+      .notNull()
+      .references(() => user.id, { onDelete: 'cascade' }),
+    clubId: text('club_id')
+      .notNull()
+      .references(() => club.id, { onDelete: 'cascade' }),
+    playerId: text('player_id')
+      .notNull()
+      .references(() => player.id, { onDelete: 'cascade' }),
+    status: text().notNull(),
+    createdAt: integer('created_at').notNull(),
+    updatedAt: integer('updated_at').notNull(),
+  },
+  (table) => [uniqueIndex('affiliation_user_club_unique_idx').on(table.userId, table.clubId)],
+);
 
-export const player = sqliteTable("player", {
-	id: text().primaryKey().notNull(),
-	nickname: text().notNull(),
-	realname: text(),
-	userId: text("user_id").references(() => user.id),
-	rating: integer().default(1500).notNull(),
-	clubId: text("club_id").notNull().references(() => club.id),
-	lastSeenAt: integer("last_seen_at").notNull(),
-	ratingPeak: integer("rating_peak").default(1500).notNull(),
-	ratingDeviation: integer("rating_deviation").default(350).notNull(),
-	ratingVolatility: integer("rating_volatility").default(0.06).notNull(),
-	ratingLastUpdateAt: integer("rating_last_update_at").notNull(),
-},
-(table) => [
-	uniqueIndex("player_user_club_unique_idx").on(table.userId, table.clubId),
-	uniqueIndex("player_nickname_club_unique_idx").on(table.nickname, table.clubId),
-]);
+export const player = sqliteTable(
+  'player',
+  {
+    id: text().primaryKey().notNull(),
+    nickname: text().notNull(),
+    realname: text(),
+    userId: text('user_id').references(() => user.id),
+    rating: integer().default(1500).notNull(),
+    clubId: text('club_id')
+      .notNull()
+      .references(() => club.id),
+    lastSeenAt: integer('last_seen_at').notNull(),
+    ratingPeak: integer('rating_peak').default(1500).notNull(),
+    ratingDeviation: integer('rating_deviation').default(350).notNull(),
+    ratingVolatility: integer('rating_volatility').default(0.06).notNull(),
+    ratingLastUpdateAt: integer('rating_last_update_at').notNull(),
+  },
+  (table) => [
+    uniqueIndex('player_user_club_unique_idx').on(table.userId, table.clubId),
+    uniqueIndex('player_nickname_club_unique_idx').on(table.nickname, table.clubId),
+  ],
+);
 
-export const game = sqliteTable("game", {
-	id: text().primaryKey().notNull(),
-	gameNumber: integer("game_number").notNull(),
-	roundNumber: integer("round_number").notNull(),
-	roundName: text("round_name"),
-	whiteId: text("white_id").notNull().references(() => player.id),
-	blackId: text("black_id").notNull().references(() => player.id),
-	whitePrevGameId: text("white_prev_game_id"),
-	blackPrevGameId: text("black_prev_game_id"),
-	result: text(),
-	tournamentId: text("tournament_id").notNull().references(() => tournament.id),
-	finishedAt: integer("finished_at"),
+export const game = sqliteTable('game', {
+  id: text().primaryKey().notNull(),
+  gameNumber: integer('game_number').notNull(),
+  roundNumber: integer('round_number').notNull(),
+  roundName: text('round_name'),
+  whiteId: text('white_id')
+    .notNull()
+    .references(() => player.id),
+  blackId: text('black_id')
+    .notNull()
+    .references(() => player.id),
+  whitePrevGameId: text('white_prev_game_id'),
+  blackPrevGameId: text('black_prev_game_id'),
+  result: text(),
+  tournamentId: text('tournament_id')
+    .notNull()
+    .references(() => tournament.id),
+  finishedAt: integer('finished_at'),
 });
 
-export const playersToTournaments = sqliteTable("players_to_tournaments", {
-	id: text().primaryKey().notNull(),
-	playerId: text("player_id").notNull().references(() => player.id),
-	tournamentId: text("tournament_id").notNull().references(() => tournament.id),
-	wins: integer().notNull(),
-	losses: integer().notNull(),
-	draws: integer().notNull(),
-	colorIndex: integer("color_index").notNull(),
-	place: integer(),
-	isOut: integer("is_out"),
-	pairingNumber: integer("pairing_number"),
-	ratingChange: integer("rating_change"),
-	ratingDeviationChange: integer("rating_deviation_change"),
-	volatilityChange: integer("volatility_change"),
+export const playersToTournaments = sqliteTable('players_to_tournaments', {
+  id: text().primaryKey().notNull(),
+  playerId: text('player_id')
+    .notNull()
+    .references(() => player.id),
+  tournamentId: text('tournament_id')
+    .notNull()
+    .references(() => tournament.id),
+  wins: integer().notNull(),
+  losses: integer().notNull(),
+  draws: integer().notNull(),
+  colorIndex: integer('color_index').notNull(),
+  place: integer(),
+  isOut: integer('is_out'),
+  pairingNumber: integer('pairing_number'),
+  ratingChange: integer('rating_change'),
+  ratingDeviationChange: integer('rating_deviation_change'),
+  volatilityChange: integer('volatility_change'),
 });
 
-export const tournament = sqliteTable("tournament", {
-	id: text().primaryKey().notNull(),
-	name: text(),
-	format: text().notNull(),
-	type: text().notNull(),
-	date: text().notNull(),
-	createdAt: integer("created_at").notNull(),
-	clubId: text("club_id").notNull().references(() => club.id),
-	startedAt: integer("started_at"),
-	closedAt: integer("closed_at"),
-	roundsNumber: integer("rounds_number"),
-	ongoingRound: integer("ongoing_round").notNull(),
-	rated: integer().notNull(),
+export const tournament = sqliteTable('tournament', {
+  id: text().primaryKey().notNull(),
+  name: text(),
+  format: text().notNull(),
+  type: text().notNull(),
+  date: text().notNull(),
+  createdAt: integer('created_at').notNull(),
+  clubId: text('club_id')
+    .notNull()
+    .references(() => club.id),
+  startedAt: integer('started_at'),
+  closedAt: integer('closed_at'),
+  roundsNumber: integer('rounds_number'),
+  ongoingRound: integer('ongoing_round').notNull(),
+  rated: integer().notNull(),
 });
 
-export const apiToken = sqliteTable("api_token", {
-	id: text().primaryKey().notNull(),
-	tokenHash: text("token_hash").notNull(),
-	userId: text("user_id").notNull().references(() => user.id, { onDelete: "cascade" } ),
-	name: text().notNull(),
-	createdAt: integer("created_at").notNull(),
-	lastUsedAt: integer("last_used_at"),
+export const apiToken = sqliteTable('api_token', {
+  id: text().primaryKey().notNull(),
+  tokenHash: text('token_hash').notNull(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id, { onDelete: 'cascade' }),
+  name: text().notNull(),
+  createdAt: integer('created_at').notNull(),
+  lastUsedAt: integer('last_used_at'),
 });
 
-export const userSession = sqliteTable("user_session", {
-	id: text().primaryKey().notNull(),
-	userId: text("user_id").notNull().references(() => user.id),
-	expiresAt: integer("expires_at").notNull(),
+export const userSession = sqliteTable('user_session', {
+  id: text().primaryKey().notNull(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id),
+  expiresAt: integer('expires_at').notNull(),
 });
 
-export const userPreferences = sqliteTable("user_preferences", {
-	userId: text("user_id").primaryKey().notNull().references(() => user.id),
-	language: text(),
+export const userPreferences = sqliteTable('user_preferences', {
+  userId: text('user_id')
+    .primaryKey()
+    .notNull()
+    .references(() => user.id),
+  language: text(),
 });
 
-export const user = sqliteTable("user", {
-	id: text().primaryKey().notNull(),
-	name: text(),
-	email: text().notNull(),
-	username: text().notNull(),
-	rating: integer(),
-	selectedClub: text("selected_club").notNull().references(() => club.id),
-	createdAt: integer("created_at").notNull(),
-},
-(table) => [
-	uniqueIndex("user_username_unique").on(table.username),
-	uniqueIndex("user_email_unique").on(table.email),
-]);
-
+export const user = sqliteTable(
+  'user',
+  {
+    id: text().primaryKey().notNull(),
+    name: text(),
+    email: text().notNull(),
+    username: text().notNull(),
+    rating: integer(),
+    selectedClub: text('selected_club')
+      .notNull()
+      .references(() => club.id),
+    createdAt: integer('created_at').notNull(),
+  },
+  (table) => [uniqueIndex('user_username_unique').on(table.username), uniqueIndex('user_email_unique').on(table.email)],
+);
--- types/ws-events.d.ts
+++ types/ws-events.d.ts
@@ -4,5 +4,4 @@ type DashboardMessage =
   | { type: 'add-existing-player'; body: PlayerModel }
   | { type: 'add-new-player'; body: PlayerModel }
   | { type: 'remove-player'; id: string } // onError add-exidsting-player
-  | {
-    type: 'set-game-result';
+  | { type: 'set-game-result' };
